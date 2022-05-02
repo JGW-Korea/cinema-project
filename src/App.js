2d262548514e7dfddd8904227d0445f06bd3479f
JGW-Korea <jgw6372@gmail.com>
2022-05-03 02:47:50 +0900

    sing up page(220503)

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import { React, useEffect, useState } from "react";
+import "./App.css";
+import Axios from "axios";
 
 function App() {
+  const [name, setName] = useState(null);
+  const [id, setId] = useState(null);
+  const [pass, setPass] = useState(null);
+  const [findId, setFindId] = useState([]);
+  const [checkId, setCheckId] = useState(false);
+
+  const idValue = (event) => {
+    setId(String(event.target.value));
+  };
+
+  const passValue = (event) => {
+    setPass(String(event.target.value));
+  };
+
+  const nameValue = (event) => {
+    setName(String(event.target.value));
+  };
+
+  const onSubmit = (event) => {
+    Axios.post("http://localhost:3002/Create/insert", {
+      createName: name,
+      createId: id,
+      createPass: pass,
+    }).then(() => {
+      alert("successful insert");
+    });
+  };
+
+  // useEffect(() => {
+  //   Axios.get("http://localhost:3002/Create/get").then((response) => {
+  //     setFindId(response.data);
+  //   });
+  // }, []);
+
+  // const idCheckTF = (event) => {
+  //   for (const i in findId) {
+  //     if (findId[i].id !== id) {
+  //       setCheckId(false);
+  //     } else {
+  //       setCheckId(true);
+  //     }
+  //   }
+  // };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Create Your email</h1>
+
+      <form>
+        <label>name</label>
+        <input type="text" value={name} onChange={nameValue} />
+        <label>id</label>
+        <input type="text" value={id} onChange={idValue} />
+
+        <label>password</label>
+        <input
+          type="password"
+          value={pass}
+          onChange={passValue}
+          disabled={checkId}
+        />
+        <button onClick={onSubmit}>Create</button>
+      </form>
     </div>
   );
 }
